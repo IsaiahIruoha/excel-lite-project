73cfd507c72b3e46fa2ef1c7f67a877889c5f794
Isaiah Iruoha <Siruoha@gmail.com>
2023-11-29 16:26:33 -0500

    update

---
+++ build/CMakeFiles/testrunner.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/isaiah/Desktop/Career/Fall 2023/ELEC 278/Final Assignment/assignment/testrunner.c" "CMakeFiles/testrunner.dir/testrunner.c.o" "gcc" "CMakeFiles/testrunner.dir/testrunner.c.o.d"
+  "/Users/isaiah/Desktop/Career/Fall 2023/ELEC 278/Final Assignment/assignment/tests.c" "CMakeFiles/testrunner.dir/tests.c.o" "gcc" "CMakeFiles/testrunner.dir/tests.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
